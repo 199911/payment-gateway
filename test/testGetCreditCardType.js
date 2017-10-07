ddf63526657c3abaca0d3e74a10190422638b4fc
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-10-07 13:26:26 +0800

    Add test on get credit card type
    Also update the logic with wiki payment card data.

---
+++ test/testGetCreditCardType.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const getCreditCardType = require('../lib/getCreditCardType.js');
+
+let expect;
+
+//
+
+expect = 'amex';
+assert(expect === getCreditCardType('378282246310005'), 'Expect amex');
+assert(expect === getCreditCardType('371449635398431'), 'Expect amex');
+
+expect = 'mastercard';
+assert(expect === getCreditCardType('5555555555554444'), 'Expect mastercard');
+assert(expect === getCreditCardType('2223000048400011'), 'Expect mastercard');
+
+expect = 'visa';
+assert(expect === getCreditCardType('4111111111111111'), 'Expect visa');
+
+expect = 'discover';
+assert(expect === getCreditCardType('6011111111111117'), 'Expect discover');
+
+expect = 'maestro';
+assert(expect === getCreditCardType('6304000000000000'), 'Expect maestro');
+
+
+console.log('tested getCreditCardType');
+
+//
