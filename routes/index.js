6628731372a1348c5814c3c192fef0aab412a739
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-10-07 14:44:11 +0800

    Add database logic to the payment form
    Also fix indentation.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const validator = require('../public/javascripts/validator.js');
 const getCreditCardType = require('../lib/getCreditCardType.js');
 const gateway = require('../lib/gateway.js');
+const db = require('../lib/db.js');
+const uuidv4 = require('uuid/v4');
 
 /* GET home page. */
 router
@@ -27,35 +29,53 @@ router
     // Gateway logic
     let paymentGateway;
     if (data['card-type'] === 'amex') {
-        if (data['currency'] === 'USD') {
-            paymentGateway = gateway.paypal;
-        } else {
-            paymentGateway = null;
-        }
+      if (data['currency'] === 'USD') {
+        paymentGateway = gateway.paypal;
+      } else {
+        paymentGateway = null;
+      }
     } else {
-        if (data['currency'] === 'USD' || data['currency'] === 'EUR' || data['currency'] === 'AUD') {
-            paymentGateway = gateway.paypal;
-        } else {
-            paymentGateway = gateway.braintree;
-        }
+      if (data['currency'] === 'USD' || data['currency'] === 'EUR' || data['currency'] === 'AUD') {
+        paymentGateway = gateway.paypal;
+      } else {
+        paymentGateway = gateway.braintree;
+      }
     }
     if (paymentGateway) {
-        paymentGateway.payAsync(data)
-            .then((res) => {
-                console.log('res');
-                console.log(JSON.stringify(res,null, 2));
-                // TODO send proper response to front end
-            })
-            .catch((err) => {
-                console.log('err');
-                console.log(err);
-                if (err.response) {
-                    console.log(err.response);
-                }
-                // TODO send proper response to front end
-            });
+      paymentGateway.payAsync(data)
+        .then(function(res) {
+          console.log('res');
+          console.log(JSON.stringify(res,null, 2));
+          return res;
+        })
+        .catch(function(err) {
+          console.log('err');
+          console.log(err);
+          if (err.response) {
+              console.log(err.response);
+          }
+          throw err;
+        })
+        .then(function(res) {
+          const record = {
+            uuid: uuidv4(),
+            name: data['customer-name'],
+            phone: data['customer-phone'],
+            currency: data['currency'],
+            price: data['price'],
+            response: JSON.stringify(res)
+          };
+          return db.insertAsync(record);
+        })
+        .then(function(){
+          // TODO update cache
+          // TODO send proper response to front end
+        })
+        .catch(function(err) {
+          // TODO send proper response to front end
+        });
     } else {
-        // TODO error handling, amex must use usd
+      // TODO error handling, amex must use usd
     }
     res.send(JSON.stringify(data, null, 4));
   });
