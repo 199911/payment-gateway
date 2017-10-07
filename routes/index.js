b4d773186520cda3b738750043719e2b94172481
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-10-07 13:40:21 +0800

    Implement draft on gateway logic

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // Reuse the front-end validation logic
 const validator = require('../public/javascripts/validator.js');
 const getCreditCardType = require('../lib/getCreditCardType.js');
+const gateway = require('../lib/gateway.js');
 
 /* GET home page. */
 router
@@ -23,7 +24,39 @@ router
     }
     data['card-type'] = getCreditCardType(data['card-number']);
     data.isValid = isValid;
-
+    // Gateway logic
+    let paymentGateway;
+    if (data['card-type'] === 'amex') {
+        if (data['currency'] === 'USD') {
+            paymentGateway = gateway.paypal;
+        } else {
+            paymentGateway = null;
+        }
+    } else {
+        if (data['currency'] === 'USD' || data['currency'] === 'EUR' || data['currency'] === 'AUD') {
+            paymentGateway = gateway.paypal;
+        } else {
+            paymentGateway = gateway.braintree;
+        }
+    }
+    if (paymentGateway) {
+        paymentGateway.payAsync(data)
+            .then((res) => {
+                console.log('res');
+                console.log(JSON.stringify(res,null, 2));
+                // TODO send proper response to front end
+            })
+            .catch((err) => {
+                console.log('err');
+                console.log(err);
+                if (err.response) {
+                    console.log(err.response);
+                }
+                // TODO send proper response to front end
+            });
+    } else {
+        // TODO error handling, amex must use usd
+    }
     res.send(JSON.stringify(data, null, 4));
   });
 
