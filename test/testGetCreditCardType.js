aa524a01cfc2036049d0a05f9a335110bad2e280
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-10-08 12:20:52 +0800

    Add more card type test cases

--- test/testGetCreditCardType.js
+++ test/testGetCreditCardType.js
@@ -11,13 +11,16 @@ assert(expect === getCreditCardType('371449635398431'), 'Expect amex');
 
 expect = 'mastercard';
 assert(expect === getCreditCardType('5555555555554444'), 'Expect mastercard');
+assert(expect === getCreditCardType('5110926052281468'), 'Expect mastercard');
 assert(expect === getCreditCardType('2223000048400011'), 'Expect mastercard');
 
 expect = 'visa';
 assert(expect === getCreditCardType('4111111111111111'), 'Expect visa');
+assert(expect === getCreditCardType('4032037666284084'), 'Expect visa');
 
 expect = 'discover';
 assert(expect === getCreditCardType('6011111111111117'), 'Expect discover');
+assert(expect === getCreditCardType('6011041778999902'), 'Expect discover');
 
 expect = 'maestro';
 assert(expect === getCreditCardType('6304000000000000'), 'Expect maestro');
